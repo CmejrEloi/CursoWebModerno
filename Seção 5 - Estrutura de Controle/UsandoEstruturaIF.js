c68faa1bbc1518543f54c9c795a5cf8cb46e2a25
Claudio <junior040712@gmail.com>
2022-09-08 18:27:08 -0300

    Estrutura IF

---
+++ Seção 5 - Estrutura de Controle/UsandoEstruturaIF.js
@@ -0,0 +1,26 @@
+function soBoaNoticia(nota){
+    if(nota >= 7){
+        console.log('Aprovado, Parabéns!!' + nota);
+    }
+}
+
+soBoaNoticia(8);
+soBoaNoticia(6.9);
+
+function seForVerdadeEuFalo(valor){
+    if(valor){
+        console.log('É verdade....' + valor);
+    }
+}
+
+seForVerdadeEuFalo(5);
+
+function teste1(num){
+    if(num > 7){
+        console.log(num);
+        console.log('Final');
+    }
+}
+
+teste1(6)
+teste1(8)
