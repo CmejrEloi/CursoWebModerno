260ec774c60faa2804540e114004562b8e8ccf2c
Claudio Eloi <junior040712@gmail.com>
2022-09-15 13:43:08 -0300

    Com CSS Finalizado

---
+++ AppPrevisaoDoTempo/js/scripts.js
@@ -0,0 +1,16 @@
+//Variaveis e seleção de elementos
+const apiKey = "6a0eeab1fd3c905d0d28cf2b9f33d174";
+const urlCountryApi = "https://countryflagsapi.com/png/";
+
+ const cityInput = document.querySelector("#city-input");
+ const searchBtn = document.querySelector("#search");
+
+
+ //Funções
+
+
+ //Eventos
+ searchBtn.addEventListener("click", (e)=>{
+    e.preventDefault();
+    console.log('Teste')
+ })
